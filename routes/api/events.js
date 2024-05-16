b6789b2a13b18ad4105aa72649660fcfa803f519
Elena Pedko <elpg.ods@gmail.com>
2024-05-16 14:06:12 +0300

    test amendments are done

--- routes/api/events.js
+++ routes/api/events.js
@@ -2,13 +2,13 @@ const express = require('express');
  
 const { getEventsController, getEventByIdController, addParticipantController } = require('../../controllers/index');
 const { asyncWrapper } = require('../../helpers/asyncWrapper');
-const { checkEventId, addParticipantValidation } = require('../../middlewares/index');
+const { checkEventId } = require('../../middlewares/index');
 
 const router = express.Router();
 
 router.get('/', asyncWrapper(getEventsController));
 router.get('/:id', checkEventId, asyncWrapper(getEventByIdController));
-router.post('/:id/registration', addParticipantValidation, asyncWrapper(addParticipantController));
+router.post('/:id/registration', asyncWrapper(addParticipantController));
 
 
 module.exports = { eventsRouter: router };
